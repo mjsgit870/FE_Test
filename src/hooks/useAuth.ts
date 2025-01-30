132df6366501007bda1ffd29faeb6ed1619bcd01
mjs <saputraj870@gmail.com>
2025-01-31 00:25:32 +0700

    refactor useAuth hooks

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,36 +1,29 @@
-import { loginService, setAuthCookie } from "@/services/auth.service";
-import { ApiError } from "@/utils/error-handler";
-import { useMutation } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
+import { loginService, setAuthCookie } from "@/services/auth.service"
+import { ApiError } from "@/utils/error-handler"
+import { useMutation } from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
+import { toast } from "react-toastify"
 
 export const useAuth = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   const loginMutation = useMutation({
     mutationFn: loginService,
     onSuccess: (data) => {
       // Set token to cookie
-      setAuthCookie(data.token);
+      setAuthCookie(data.token)
 
       // Redirect to dashboard
-      router.push("/dashboard");
+      router.push("/dashboard")
     },
     onError: (error) => {
-      if (error instanceof ApiError && error.status === 422) {
-        const { errors } = error;
-        const firstError = errors ? Object.values(errors)[0] : "Unknown error";
-
-        return toast(firstError, { type: "error", theme: "colored" });
-      }
-
-      return toast(error.message, { type: "error", theme: "colored" });
+      toast(error.message, { type: "error", theme: "colored" })
     },
-  });
+  })
 
   return {
     login: loginMutation.mutate,
     isLoading: loginMutation.isPending,
     error: loginMutation.error,
-  };
-};
+  }
+}
