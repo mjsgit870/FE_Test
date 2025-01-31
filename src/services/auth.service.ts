b1ebacb4934e7ff766a515c270e8ba8f3db1b367
mjs <saputraj870@gmail.com>
2025-01-31 23:30:27 +0700

    add logout service api

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,30 +1,41 @@
-import api from "@/lib/axios"
-import { deleteCookie, getCookie, setCookie } from "cookies-next"
+import api from "@/lib/axios";
+import { deleteCookie, getCookie, setCookie } from "cookies-next";
 
 export interface LoginCredentials {
-  username: string
-  password: string
+  username: string;
+  password: string;
 }
 
 interface LoginResponse {
-  token: string
+  access_token: string;
+  expires_in: number;
 }
 
-export const loginService = async (
-  credentials: LoginCredentials
-): Promise<LoginResponse> => {
-  const res = await api.post<{ data: LoginResponse }>("/login", credentials)
-  return res.data.data
+interface LogoutResponse {
+  message: string;
 }
 
-export const setAuthCookie = (token: string) => {
-  setCookie("auth_token", token)
-}
+export const loginService = async (credentials: LoginCredentials): Promise<LoginResponse> => {
+  const res = await api.post<LoginResponse>("/login", credentials);
+  return res.data;
+};
 
-export const getAuthCookie = (token: string) => {
-  getCookie("auth_token")
-}
+export const logoutService = async (): Promise<LogoutResponse> => {
+  await new Promise((r) => setTimeout(r, 2000));
+  const res = await api.post<LogoutResponse>("/logout");
+  return res.data;
+};
+
+export const setAuthCookie = (token: string, expires_at: number) => {
+  setCookie("auth_token", token, {
+    maxAge: expires_at,
+  });
+};
+
+export const getAuthCookie = () => {
+  getCookie("auth_token");
+};
 
 export const removeAuthCookie = () => {
-  deleteCookie("auth_token")
-}
+  deleteCookie("auth_token");
+};
