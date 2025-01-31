4da104946437ab7909b16bf8c0ff20d26c0ba454
mjs <saputraj870@gmail.com>
2025-01-31 23:31:35 +0700

    refacotr useAuth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,29 +1,28 @@
-import { loginService, setAuthCookie } from "@/services/auth.service"
-import { ApiError } from "@/utils/error-handler"
-import { useMutation } from "@tanstack/react-query"
-import { useRouter } from "next/navigation"
-import { toast } from "react-toastify"
+import { loginService, setAuthCookie } from "@/services/auth.service";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
 
 export const useAuth = () => {
-  const router = useRouter()
+  const router = useRouter();
 
   const loginMutation = useMutation({
     mutationFn: loginService,
     onSuccess: (data) => {
       // Set token to cookie
-      setAuthCookie(data.token)
+      setAuthCookie(data.access_token, data.expires_in);
 
       // Redirect to dashboard
-      router.push("/dashboard")
+      router.push("/dashboard");
     },
     onError: (error) => {
-      toast(error.message, { type: "error", theme: "colored" })
+      toast(error.message, { type: "error" });
     },
-  })
+  });
 
   return {
     login: loginMutation.mutate,
     isLoading: loginMutation.isPending,
     error: loginMutation.error,
-  }
-}
+  };
+};
