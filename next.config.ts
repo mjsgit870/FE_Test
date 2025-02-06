7a1f8cf77889816f4060b33947bd34fb4cea7cd6
mjs <saputraj870@gmail.com>
2025-02-06 19:57:26 +0700

    skip eslint while building

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
-import type { NextConfig } from "next"
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
-}
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
