1c4b8de9bd1801437dfa93f5f539ab40195ba853
mjs <saputraj870@gmail.com>
2025-02-06 10:50:20 +0700

    prevent while data ruas not found

--- src/app/(authenticated)/master-data/[id]/view/page.tsx
+++ src/app/(authenticated)/master-data/[id]/view/page.tsx
@@ -1,18 +1,28 @@
 "use client";
 
 import { useGetRuas } from "@/features/master-data/api/useGetRuas";
+import { ApiError } from "@/utils/error-handler";
 import { Button, Container, Text } from "@mantine/core";
 import { IconArrowLeft } from "@tabler/icons-react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 const ViewRuasPage = dynamic(() => import("@/features/master-data/routes/ViewRuasPage"));
 
 export default function ViewRuas() {
   const params = useParams<{ id: string }>();
 
-  const { data: ruas, isPending, isFetching } = useGetRuas({ id: params.id });
+  const { data: ruas, isPending, isFetching, isError, error } = useGetRuas({ id: params.id });
+
+  useEffect(() => {
+    if (isError && error instanceof ApiError && error.status === 404) {
+      toast.error("Ruas tidak ditemukan");
+      return redirect("/master-data");
+    }
+  }, [isError]);
 
   return (
     <Container p={0}>
